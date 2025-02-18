24e289b5e82e5afcaeff1ef026a96d203ca85668
Beatrice <beatricesim31@gmail.com>
2025-02-18 09:07:14 +0800

    small changes

--- src/app/components/inventory/inventory.component.ts
+++ src/app/components/inventory/inventory.component.ts
@@ -10,8 +10,8 @@ import { CartEvent } from '../../models';
 })
 export class InventoryComponent {
   images = [ '/fruits/apple.png', '/fruits/carrot.png', '/fruits/broccoli.png', '/fruits/mushroom.png', '/fruits/pumpkin.png']
-  descs: String[] = [ 'Apple', 'Carrot', 'Broccoli', 'Mushroom', 'Pumpkin']
-  prices: number[] = [ 1.25, 0.65, 1.4, 1.5, 4.5 ]
+  descs = [ 'Apple', 'Carrot', 'Broccoli', 'Mushroom', 'Pumpkin']
+  prices  = [ 1.25, 0.65, 1.4, 1.5, 4.5 ]
 
   @Output()
   addItem = new Subject<CartEvent>()
